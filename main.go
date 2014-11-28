0039bcf393255675e8c20766e8bd587e12eaa77f
Mike Dewar <mikedewar@gmail.com>
2014-11-28 18:01:15 -0500

    make all power values between 0 and 255

--- main.go
+++ main.go
@@ -10,40 +10,83 @@ import (
 	"os/signal"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // scan a range of the spectrum, emitting the result periodically. This just wraps http://kmkeen.com/rtl-power/index.html
 func scan(outChan chan []byte, quitChan chan bool) {
-	cmd := exec.Command("/usr/local/bin/rtl_power", "-f", "118M:137M:8k", "-g", "50", "-i", "10")
+	cmd := exec.Command("/usr/local/bin/rtl_power", "-f", "118M:150M:8k", "-g", "50", "-i", "5")
 	stdout, _ := cmd.StdoutPipe()
 	if err := cmd.Start(); err != nil {
 		log.Fatal(err)
 	}
 	reader := bufio.NewReader(stdout)
+	var t, τ time.Time
+	data := make([]float64, 0)
+	//2014-11-28, 13:23:14
+	//Mon Jan 2 15:04:05 -0700 MST 2006
+	layout := "2006-01-02 15:04:05"
 	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-		dstring := strings.Split(line, ",")[7:]
-		d := make([]float64, len(dstring))
-		for i, ds := range dstring {
-			d[i], err = strconv.ParseFloat(strings.TrimSpace(ds), 64)
-			if err != nil {
-				log.Fatal(err)
+	Concatenate:
+		for {
+			select {
+			default:
+				line, err := reader.ReadString('\n')
+				if err != nil {
+					log.Fatal(err)
+				}
+				lineArray := strings.Split(line, ",")
+				t, err = time.Parse(layout, lineArray[0]+lineArray[1])
+				if err != nil {
+					log.Fatal(err)
+				}
+				if τ.IsZero() {
+					τ = t
+				}
+				if t != τ {
+					min := data[0]
+					for _, value := range data {
+						if value < min {
+							min = value
+						}
+					}
+					for i, _ := range data {
+						data[i] -= min
+					}
+					max := data[0]
+					for _, value := range data {
+						if value > max {
+							max = value
+						}
+					}
+					scale := 255.0 / max
+					for i, _ := range data {
+						data[i] *= scale
+					}
+					out, err := json.Marshal(data)
+					if err != nil {
+						log.Fatal(err)
+					}
+					outChan <- out
+					data = make([]float64, 0)
+					τ = t
+					break Concatenate
+				}
+				dstring := lineArray[7:]
+				d := make([]float64, len(dstring))
+				for i, ds := range dstring {
+					d[i], err = strconv.ParseFloat(strings.TrimSpace(ds), 64)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+				data = append(data, d...)
+				τ = t
+			case <-quitChan:
+				return
 			}
 		}
-		out, err := json.Marshal(d)
-		if err != nil {
-			log.Fatal(err)
-		}
-		select {
-		case outChan <- out:
-		case <-quitChan:
-			return
-		}
 	}
-
 }
 
 func main() {
@@ -62,6 +105,7 @@ func main() {
 	go wsServer.websocketRouter()
 	go scan(wsServer.broadcast, quitChan)
 
+	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.HandleFunc("/ws", wsServer.websocketHandler)
 	log.Println("serving rtl_power websocket on 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
